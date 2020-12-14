e3bfe94b4802eb9bd050d6c8585f0224f9ce4d5d
jangidsampat <sampatlal.jangid18@vit.edu>
2020-12-14 18:45:21 +0530

    Changes

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SIH.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CNTCP.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- sihModel/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SihmodelConfig(AppConfig):
-    name = 'sihModel'
